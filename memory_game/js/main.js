fda468888c6f560535ad6204e83436790d90fcda
jayhathaway <sexyalterego+git@gmail.com>
2018-05-03 15:21:14 -0700

    updated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+  console.log("User flipped " + cardsInPlay[0]);
+  console.log("User flipped " + cardsInPlay[1]);
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("Winner, winner, chicken dinner!");
+  } else {
+    alert("Sorry. Try again!");
+  }
+}
