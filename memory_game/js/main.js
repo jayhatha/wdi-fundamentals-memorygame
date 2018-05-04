4746e9354066e0960513078f3d1d9e3ab3bfafc7
jayhathaway <sexyalterego+git@gmail.com>
2018-05-03 17:06:56 -0700

    updated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,22 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) {
-  console.log("User flipped " + cardsInPlay[0]);
-  console.log("User flipped " + cardsInPlay[1]);
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("Winner, winner, chicken dinner!");
-  } else {
-    alert("Sorry. Try again!");
+
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert('Winner, winner, chicken dinner!');
+    } else {
+      alert('Sorry. Try again!');
+    }
   }
 }
+
+function flipCard(cardId) {
+
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+  console.log("User flipped " + cards[cardId]);
+
+}
+flipCard(0);
+flipCard(2);
