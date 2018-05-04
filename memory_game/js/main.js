6718702d400b866dbe31c68c214367449b606cf5
jayhathaway <sexyalterego+git@gmail.com>
2018-05-04 16:49:54 -0700

    updated flipcard function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png'
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'images/king-of-diamonds.png'
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png'
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -13,9 +34,10 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
 
 }
 flipCard(0);
