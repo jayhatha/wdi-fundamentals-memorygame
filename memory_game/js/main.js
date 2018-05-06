4ac929f273f584b1f86484a02b5da253acf9931e
jayhathaway <sexyalterego+git@gmail.com>
2018-05-05 22:04:20 -0700

    finished game logic and added reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,28 +1,44 @@
-var cards = [
-{
-rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
-},
-{
-rank: 'queen',
-suit: 'diamonds',
-cardImage: 'images/queen-of-diamonds.png'
-},
-{
-rank: 'king',
-suit: 'diamonds',
-cardImage: 'images/king-of-diamonds.png'
-},
-{
-rank: 'king',
-suit: 'hearts',
-cardImage: 'images/king-of-hearts.png'
-}
+// setting up the cards
+var cards = [{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  }
 ];
-var cardsInPlay = [];
+// creating the board
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
 
+}
+
+
+// storing the cards the player picks
+var cardsInPlay = [];
+var suitsInPlay = [];
+//checking for a match
 function checkForMatch() {
+
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       alert('Winner, winner, chicken dinner!');
@@ -31,14 +47,16 @@ function checkForMatch() {
     }
   }
 }
-
-function flipCard(cardId) {
-
+// flipping over the selected cards
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
+  this.setAttribute('src', cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
-  checkForMatch();
+  suitsInPlay.push(cards[cardId].suit);
   console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
   console.log(cards[cardId].cardImage);
-
+  checkForMatch();
 }
-flipCard(0);
-flipCard(2);
+
+
+createBoard();
