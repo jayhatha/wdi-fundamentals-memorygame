6662e8a7ba618e601ae9740b0942c2441011b766
jayhathaway <sexyalterego+git@gmail.com>
2018-05-06 13:32:14 -0700

    finished game logic with random cards, scoreboard, try again button and reset score button. woo

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,6 +26,7 @@ function createBoard() {
     var cardElement = document.createElement('img');
     cardElement.setAttribute('src', 'images/back.png');
     cardElement.setAttribute('data-id', i);
+    cardElement.setAttribute('flipped', false);
     cardElement.addEventListener('click', flipCard);
     document.getElementById('game-board').appendChild(cardElement);
   }
@@ -35,28 +36,52 @@ function createBoard() {
 
 // storing the cards the player picks
 var cardsInPlay = [];
-var suitsInPlay = [];
+// setting up the scoreboard
+var score = 0;
+var attempts = 0;
+
 //checking for a match
 function checkForMatch() {
 
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       alert('Winner, winner, chicken dinner!');
+      attempts++;
+      score ++;
+      document.getElementById("scoreboard").innerHTML = ('Score: ' + score + ' ' + ((score/attempts)*100).toFixed(1) + '%');
     } else {
       alert('Sorry. Try again!');
+      attempts ++;
+      document.getElementById("scoreboard").innerHTML = ('Score: ' + score + ' ' + ((score/attempts)*100).toFixed(1) + '%');
     }
   }
 }
 // flipping over the selected cards
 function flipCard() {
   var cardId = this.getAttribute('data-id');
-  this.setAttribute('src', cards[cardId].cardImage);
-  cardsInPlay.push(cards[cardId].rank);
-  suitsInPlay.push(cards[cardId].suit);
-  console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
-  console.log(cards[cardId].cardImage);
-  checkForMatch();
+  var isFlipped = this.getAttribute('src');
+  console.log(isFlipped);
+  if (isFlipped === 'images/back.png') {
+    this.setAttribute('src', cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
+    checkForMatch();
+  } else {
+    console.log('card was already flipped');
+  }
 }
-
-
+// resetting the board
+function resetCards() {
+  document.getElementById('game-board').innerHTML = "";
+  cardsInPlay = [];
+  cards.sort(function() { return 0.5 - Math.random() });
+  createBoard();
+}
+function resetScore() {
+  score = 0;
+  attempts = 0;
+  document.getElementById("scoreboard").innerHTML = ('Score: ');
+}
+cards.sort(function() { return 0.5 - Math.random() });
 createBoard();
